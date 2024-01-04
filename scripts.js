5b250e463dc4c44b7dc86d85a905b58ac58201f0
Ripca <ricardoarrecisrivera@gmail.com>
2024-01-04 10:27:09 -0600

    Se agregan el apartado de los idiomas y se van agregando los proyectos

--- scripts.js
+++ scripts.js
@@ -7,9 +7,32 @@ const toggleColors = document.getElementById("toggle-colors");
 //Le asignamos el elemento styles del documento a la constante rootStyles para luego poder cambiar el valor de la variable principal
 const rootStyles = document.documentElement.style;
 
+const flagsElement = document.getElementById("flags");
+const textsToChange = document.querySelectorAll("[data-section]");
+
+//Cambiamos el texto con esta funcion llamando al archivo JSON correspondiente al idioma que eligio el usuario
+const changeLanguage = async (language) => {
+    const requestJson = await fetch(`./languages/${language}.json`);
+    texts = await requestJson.json();
+
+    for (const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+        textToChange.innerHTML = texts[section][value];
+    }
+};
+
+//Evento que cambia el idioma dependiendo de si se presiona una u otra etiqueta
+flagsElement.addEventListener("click", (e) => {
+    changeLanguage(e.target.dataset.language);
+});
+
 //Seteamos la propiedad, que es nuestra variable al color que el usuario coloque cuando haga clic en alguno de los cuadros de los colores
 toggleColors.addEventListener("click", (e) => {
-    rootStyles.setProperty("--primary-color", e.target.dataset.color);
+    if (e.target.dataset.color != null) {
+        rootStyles.setProperty("--primary-color", e.target.dataset.color);
+    }
 });
 
 //Funcion para cambiar el toggle-theme (los colores principales, el icono y los textos)
@@ -17,9 +40,9 @@ toggleTheme.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     if (toggleIcon.src.includes("moon.svg")) {
         toggleIcon.src = "assets/icons/sun.svg";
-        toggleText.textContent = "Light Mode";
+        toggleText.textContent = "Light ";
     } else {
         toggleIcon.src = "assets/icons/moon.svg";
-        toggleText.textContent = "Dark Mode";
+        toggleText.textContent = "Dark";
     }
 });
